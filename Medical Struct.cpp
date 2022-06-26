d05a439a0917ca35cda17f4a427e4b7be3466b8b
Amrdnlv <onne.onne@mail.ru>
2022-06-26 23:40:15 +0300

    Добавьте файлы проекта.

---
+++ Medical Struct.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+struct medical_card
+{
+    int id_card;
+    string FIO;
+    int poll;
+    int telephone;
+    string adress;
+    string work_place;
+    string profession;
+};
+enum pol
+{
+    m,
+    zh,
+    bi
+};
+
+
+int main()
+{
+    medical_card PopovVA;
+    PopovVA.id_card = 1;
+    PopovVA.FIO= "Popov Victor Vasilevich";
+    PopovVA.telephone = 8937483232;
+    PopovVA.adress = "Kalatushkina d 12";
+    PopovVA.work_place = "Kazan MALL";
+    PopovVA.profession = "Developer";
+    PopovVA.poll = pol::bi;
+
+    cout << PopovVA.id_card << endl;
+    cout << PopovVA.FIO << endl;
+    cout << PopovVA.telephone << endl;
+    cout << PopovVA.adress << endl;
+    cout << PopovVA.work_place << endl;
+    cout << PopovVA.profession << endl;
+    cout << PopovVA.poll<< endl;
+
+
+}
